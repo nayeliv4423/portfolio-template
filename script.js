10c27a09f39ee92f45aa96d8637bd2221b544143
nayeliv4423 <73562891+nayeliv4423@users.noreply.github.com>
2020-10-28 07:12:25 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-let ownerName = "Your Name"
-let userName = "YourUsername";
+let ownerName = "Nayeli V"
+let userName = "nayeliv4423";
 
 document.querySelectorAll(".owner-name").forEach((e) => {
     e.innerHTML = ownerName;
